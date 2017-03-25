314e80b2febe56a6cddc4cb4f1b764b2ab95ed98
Karen Muradyan <karen@accontech.com>
2017-03-25 15:26:15 +0400

    Built on the ios device.

--- Classes/LinphoneUI/UIAvatarPresence.m
+++ Classes/LinphoneUI/UIAvatarPresence.m
@@ -53,27 +53,27 @@ INIT_WITH_COMMON_CF {
 }
 
 - (void)updatePresenceImage {
-	LinphonePresenceBasicStatus basic =
-		_friend ? linphone_presence_model_get_basic_status(linphone_friend_get_presence_model(_friend))
-				: LinphonePresenceBasicStatusClosed;
-	const LinphonePresenceModel *model = _friend ? linphone_friend_get_presence_model(_friend) : NULL;
-	LinphonePresenceActivity *activity =
-		model ? linphone_presence_model_get_activity(model) ?: LinphonePresenceActivityOffline : NULL;
-
-	LOGE(@"Friend %s status is now %s/%s since %@", _friend ? linphone_friend_get_name(_friend) : "NULL",
-		 basic == LinphonePresenceBasicStatusOpen ? "open" : "closed", linphone_presence_activity_to_string(activity),
-		 [NSDate dateWithTimeIntervalSince1970:linphone_presence_model_get_timestamp(model)]);
-
-	NSString *imageName;
-	if (basic == LinphonePresenceBasicStatusClosed) {
-		imageName =
-			(_friend && linphone_friend_is_presence_received(_friend)) ? @"presence_away" : @"presence_unregistered";
-	} else if (linphone_presence_activity_get_type(activity) == LinphonePresenceActivityTV) {
-		imageName = @"presence_online";
-	} else {
-		imageName = @"presence_away";
-	}
-	_presenceImage.image = [UIImage imageNamed:imageName];
+//	LinphonePresenceBasicStatus basic =
+//		_friend ? linphone_presence_model_get_basic_status(linphone_friend_get_presence_model(_friend))
+//				: LinphonePresenceBasicStatusClosed;
+//	const LinphonePresenceModel *model = _friend ? linphone_friend_get_presence_model(_friend) : NULL;
+//	LinphonePresenceActivity *activity =
+//		model ? linphone_presence_model_get_activity(model) ?: LinphonePresenceActivityOffline : NULL;
+//
+//	LOGE(@"Friend %s status is now %s/%s since %@", _friend ? linphone_friend_get_name(_friend) : "NULL",
+//		 basic == LinphonePresenceBasicStatusOpen ? "open" : "closed", linphone_presence_activity_to_string(activity),
+//		 [NSDate dateWithTimeIntervalSince1970:linphone_presence_model_get_timestamp(model)]);
+//
+//	NSString *imageName;
+//	if (basic == LinphonePresenceBasicStatusClosed) {
+//		imageName =
+//			(_friend && linphone_friend_is_presence_received(_friend)) ? @"presence_away" : @"presence_unregistered";
+//	} else if (linphone_presence_activity_get_type(activity) == LinphonePresenceActivityTV) {
+//		imageName = @"presence_online";
+//	} else {
+//		imageName = @"presence_away";
+//	}
+//	_presenceImage.image = [UIImage imageNamed:imageName];
 }
 
 - (void)setFriend:(LinphoneFriend *) friend {
